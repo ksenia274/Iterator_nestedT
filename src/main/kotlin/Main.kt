c6f8a49925c12d6b2600fa5c93a1e977e70be149
ksenia274 <kcen,yackubowa@2013@gmail.com>
2023-05-26 23:42:13 +0300

    Last

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,3 +5,37 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
+
+class Nested<T>(private val list: List<Any?>) : Iterable<T> {
+    override fun iterator(): Iterator<T> {
+        return NestedIterator(list.iterator()) as Iterator<T>
+    }
+
+    private class NestedIterator(private var iterator: Iterator<Any?>) : Iterator<Any?> {
+        private var currentIterator: NestedIterator? = null
+        private var currentItem: Any? = null
+
+        override fun hasNext(): Boolean {
+            while (currentItem == null && iterator.hasNext()) {
+                val next = iterator.next()
+                if (next is List<*>) {
+                    currentIterator = NestedIterator(next.iterator())
+                    currentItem = currentIterator?.next()
+                } else {
+                    currentItem = next
+                }
+            }
+            return currentItem != null
+        }
+
+        override fun next(): Any {
+            if (!hasNext()) throw NoSuchElementException()
+            val result = currentItem
+            currentItem = null
+            if (currentIterator?.hasNext() == true) {
+                currentItem = currentIterator?.next()
+            }
+            return result!!
+        }
+    }
+}
--- src/test/kotlin/test.kt
+++ src/test/kotlin/test.kt
@@ -1,2 +1,78 @@
-class test {
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+
+class NestedTest {
+    @Test
+    fun testNestedIterator() {
+        val list = listOf(1, listOf(2, 3), listOf(4, listOf(5, 6)))
+        val nested = Nested<Int>(list)
+
+        val expectedList = listOf(1, 2, 3, 4, 5, 6)
+        val actualList = mutableListOf<Int>()
+        for (item in nested) {
+            actualList.add(item)
+        }
+
+        assertEquals(expectedList, actualList)
+    }
+
+    @Test
+    fun testEmptyList() {
+        val list = emptyList<Any>()
+        val nested = Nested<Int>(list)
+
+        assertFalse(nested.iterator().hasNext())
+    }
+
+    @Test
+    fun testFlatList() {
+        val list = listOf(1, 2, 3)
+        val nested = Nested<Int>(list)
+
+        val expectedList = listOf(1, 2, 3)
+        val actualList = mutableListOf<Int>()
+        for (item in nested) {
+            actualList.add(item)
+        }
+
+        assertEquals(expectedList, actualList)
+    }
+
+    @Test
+    fun testNestedWithStrings() {
+        val list = listOf("hello", "world", listOf("nested", "list"), "!")
+        val nested = Nested<String>(list)
+        val expected = listOf("hello", "world", "nested", "list", "!")
+        val actual = nested.toList()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testNestedWithBooleans() {
+        val list = listOf(true, false, listOf(true, listOf(false)), true)
+        val nested = Nested<Boolean>(list)
+        val expected = listOf(true, false, true, false, true)
+        val actual = nested.toList()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testNestedWithMixedTypes() {
+        val list = listOf(1, "hello", null, listOf(true, false))
+        val nested = Nested<Any?>(list)
+        val expected = listOf(1, "hello", true, false)
+        val actual = nested.filterNotNull().toList()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testNestedWithLargeList() {
+        val list = (1..100000).toList()
+        val nested = Nested<Int>(list)
+        val expected = (1..100000).toList()
+        val actual = nested.toList()
+        assertEquals(expected, actual)
+    }
+
 }
